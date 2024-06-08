fc5a93b0087753967274fc986cc26f0668022b4e
NaulEpilef <lfps1999@gmail.com>
2024-06-08 19:59:38 -0300

    Creating SwaggerConfig

---
+++ src/main/java/dev/naul/gestao_vagas/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package dev.naul.gestao_vagas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestão de Vagas")
+                        .description("API Responsáve pela gestão de vagas")
+                        .version("1"))
+                .schemaRequirement("jwt_auth", createSecurityScheme());
+        // This way it will add authorization to all routes
+        // .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        // .components(new Components().addSecuritySchemes("Bearer Authentication",
+        // createSecurityScheme()))
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer")
+                .bearerFormat("JWT");
+    }
+}
