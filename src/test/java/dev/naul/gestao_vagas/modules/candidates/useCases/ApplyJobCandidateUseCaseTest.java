cd22fed0bb6bfc3cad7143fa8dc0d033757ca7ce
NaulEpilef <lfps1999@gmail.com>
2024-06-08 22:26:33 -0300

    Creating unit test for when idJob is null

--- src/test/java/dev/naul/gestao_vagas/modules/candidates/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/dev/naul/gestao_vagas/modules/candidates/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,11 @@
 package dev.naul.gestao_vagas.modules.candidates.useCases;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.naul.gestao_vagas.exceptions.JobNotFoundException;
 import dev.naul.gestao_vagas.exceptions.UserNotFoundException;
+import dev.naul.gestao_vagas.modules.candidates.CandidateEntity;
 import dev.naul.gestao_vagas.modules.candidates.CandidateRepository;
 import dev.naul.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -31,6 +38,23 @@ public class ApplyJobCandidateUseCaseTest {
             this.applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("Should not be able to apply job with candidate not found")
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(this.candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try {
+            this.applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }
